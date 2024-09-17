e16ce204c45fc6318f0b651a3cc1c948fcdd10a7
Ben <benjaminbt.617@gmail.com>
2024-09-17 10:38:23 -0400

    Homework: Volume - Due: 10/02/2024

---
+++ Volume_bt.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Volume_bt {
+    private static final double PI = Math.PI;
+    private static final double gallonsPerCubicFoot = 7.48;
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        double radius = getValidDoubleInput(input, "Please enter the radius of the circle (in feet): " );
+        double height = getValidDoubleInput(input, "Please enter the height of the cylinder (in feet): ");
+        double volumeCubicFeet = PI * Math.pow(radius, 2) * height; // v = pi*r^2*h
+        double volumeGallons = volumeCubicFeet / gallonsPerCubicFoot; // v = pi*r^2*h / 7.48
+        System.out.printf("The volume of the cylinder is %.2f gallons.%n", volumeGallons);
+        input.close();
+
+    } // main method
+    private static double getValidDoubleInput(Scanner input, String prompt){
+        Double value;
+        while(true){
+            System.out.println(prompt);
+            try{
+                value = Double.parseDouble(input.nextLine());
+                if (value >= 0){
+                    return value;
+                } // if
+                System.out.printf("Please enter a value larger than %.0f%n", value);
+                }/*try*/ catch (NumberFormatException e){
+                    System.out.println("Please enter valid number values only.");
+            } // catch
+        } // while
+        } //getValidDoubleInput method
+
+} // Volume_bt class
